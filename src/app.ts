3a1fae8bfbb199718ca27530c60227bd43cbf63d
shogo4131 <shougo08697118@gmail.com>
2022-11-04 15:18:52 +0900

    feat: add cookie setting

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,13 @@
 import express, { Request, Response, ErrorRequestHandler } from "express";
 import userRouters from "./routes/user";
 import authRouters from "./routes/auth";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", userRouters);
 app.use("/api/auth", authRouters);
-
-app.use((err: any, req: Request, res: Response) => {
-  const status = err.body || 500;
-  const message = err.body || "server error";
-
-  return res.status(status).json({
-    success: false,
-    status,
-    message,
-  });
-});
 
 export default app;
